d7ee5059cbb040b0153041c6bc18a904c2380860
Piyush3998 <piyush.pinak007@gmail.com>
2024-10-27 21:10:48 +1100

    ola

---
+++ SoftwareNow/pattern_softNow.py
@@ -0,0 +1,12 @@
+def create_pattern():
+    for i in range(5):
+        # Left section: @ and #
+        left_section = "@ " * (i + 1) + "# " * (5 - i - 1)
+        
+        # Right section: * and %
+        right_section = "* " * (5 - i) + "% " * i
+        
+        # Print both sections side by side
+        print(left_section + "   " + right_section)
+
+create_pattern()
